598df86f880e3e2fa4c34e05823b48c314ac558d
ATournel <a.tournel@hotmail.fr>
2018-08-05 15:18:30 +0200

    Added Jeep and Hovercraft classes variables and methods

--- Exos Interface/src/exos3/Hovercraft.java
+++ Exos Interface/src/exos3/Hovercraft.java
@@ -2,4 +2,49 @@ package exos3;
 
 public class Hovercraft implements IsLandVehicule, IsSeaVessel{
 
+	String name;
+	int maxPassengers;
+	int maxSpeed;
+	int numWheels;
+	int displacement;
+	
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public int getMaxPassengers() {
+		return maxPassengers;
+	}
+	public void setMaxPassengers(int maxPassengers) {
+		this.maxPassengers = maxPassengers;
+	}
+	public int getMaxSpeed() {
+		return maxSpeed;
+	}
+	public void setMaxSpeed(int maxSpeed) {
+		this.maxSpeed = maxSpeed;
+	}
+	public int getNumWheels() {
+		return numWheels;
+	}
+	public void setNumWheels(int numWheels) {
+		this.numWheels = numWheels;
+	}
+	public int getDisplacement() {
+		return displacement;
+	}
+	public void setDisplacement(int displacement) {
+		this.displacement = displacement;
+	}
+	
+	public void enterLand() {
+		System.out.println("is getting out of water.");
+	}
+	
+	public void enterSea() {
+		System.out.println("is getting into the water.");
+	}
+	
 }
--- Exos Interface/src/exos3/Jeep.java
+++ Exos Interface/src/exos3/Jeep.java
@@ -2,4 +2,39 @@ package exos3;
 
 public class Jeep implements IsLandVehicule{
 
+	String name;
+	int maxPassengers;
+	int maxSpeed;
+	int numWheels;
+	
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public int getMaxPassengers() {
+		return maxPassengers;
+	}
+	public void setMaxPassengers(int maxPassengers) {
+		this.maxPassengers = maxPassengers;
+	}
+	public int getMaxSpeed() {
+		return maxSpeed;
+	}
+	public void setMaxSpeed(int maxSpeed) {
+		this.maxSpeed = maxSpeed;
+	}
+	public int getNumWheels() {
+		return numWheels;
+	}
+	public void setNumWheels(int numWheels) {
+		this.numWheels = numWheels;
+	}
+	
+	public void soundHorn() {
+		System.out.println("Beeep!");
+	}
+	
+	
 }
